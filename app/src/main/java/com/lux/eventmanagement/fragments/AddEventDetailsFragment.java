383d32dd50d0881a625f60496bd583d195b19ea0
annakaimlettu@gmail.com <annakaimlettu@gmail.com>
2020-04-18 15:37:36 +0200

    error fix

--- app/src/main/java/com/lux/eventmanagement/fragments/AddEventDetailsFragment.java
+++ app/src/main/java/com/lux/eventmanagement/fragments/AddEventDetailsFragment.java
@@ -2,6 +2,7 @@ package com.lux.eventmanagement.fragments;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
@@ -11,25 +12,24 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
@@ -38,15 +38,13 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.lux.eventmanagement.R;
 import com.lux.eventmanagement.Utils;
 import com.lux.eventmanagement.adapter.EntryDetails;
+import com.lux.eventmanagement.adapter.LocationTrack;
 import com.michaelmuenzer.android.scrollablennumberpicker.ScrollableNumberPicker;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -55,25 +53,23 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
-import java.util.UUID;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 
+public class AddEventDetailsFragment extends Fragment {
 
-
-
-public class AddFoodDetailsFragment extends Fragment {
-
-    private static final String TAG = "Rudddddddddddd >.>";
+    private static final String TAG = "ggg >.>";
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
 
     private final int PICK_IMAGE_REQUEST = 10; //71 img
-    private Button btnChoose, btnUpload; //img
     private ImageView imageView; //img
     private Uri filePath; //img
     String imageUrl;
@@ -104,6 +100,20 @@ public class AddFoodDetailsFragment extends Fragment {
     EditText description, title;
     private Button btnGet, time_buttn, save;
 
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList<String> permissionsRejected = new ArrayList<>();
+    private ArrayList<String> permissions = new ArrayList<>();
+
+    private final static int ALL_PERMISSIONS_RESULT = 101;
+    LocationTrack locationTrack;
+    Button  locationbtn;
+    double longitude ;
+    double latitude ;
+    private Bitmap bitmap;
+    private File destination = null;
+    private InputStream inputStreamImg;
+    private String imgPath = null;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -124,23 +134,16 @@ public class AddFoodDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.layout_addfood, container, false);
+        View view = inflater.inflate(R.layout.layout_addevent, container, false);
 
 
         description = view.findViewById(R.id.description);
         title = view.findViewById(R.id.title);
-
-
-
-
+        locationbtn = view.findViewById(R.id.locationbtn);
 
         mprofileUserData = Utils.userDetailsFromPreference(getActivity());
-
-
-
         //Initialize Views
-        btnChoose = (Button) view.findViewById(R.id.btnChoose);
-        btnUpload = (Button) view.findViewById(R.id.btnUpload);
+
         imageView = (ImageView) view.findViewById(R.id.imgView);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -166,41 +169,88 @@ public class AddFoodDetailsFragment extends Fragment {
 
 
                 //I have add User Profile data so that we will get whole user details
-                EntryDetails details = new EntryDetails(  Utils.getUserGmail(getActivity()),des,  imageUrl  ,  tle,  user);
-                save.setText("clicked");
+                EntryDetails details = new EntryDetails(  Utils.getUserGmail(getActivity()),des,  imageUrl  ,  tle, longitude,longitude, user);
+                save.setText("Uploading...");
                 //notifyDataSetChanged();
                 saveData(details);
             }
         });
+
+        permissions.add(ACCESS_FINE_LOCATION);
+        permissions.add(ACCESS_COARSE_LOCATION);
+
+        permissionsToRequest = findUnAskedPermissions(permissions);
+        //get the permissions we have asked for before but are not granted..
+        //we will store this in a global list to access later.
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+
+            if (permissionsToRequest.size() > 0)
+                requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]), ALL_PERMISSIONS_RESULT);
+        }
+
+
+        locationbtn.setText("Share Location");
+        locationbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                locationTrack = new LocationTrack(getActivity());
+                if(locationbtn.getText().toString().contains("Share Location")) {
+                    if (locationTrack.canGetLocation()) {
+
+                        locationbtn.setText("Cancel");
+
+                        longitude = locationTrack.getLongitude();
+                        latitude = locationTrack.getLatitude();
+
+                        //Toast.makeText(getActivity(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_SHORT).show();
+                    } else {
+
+                        locationbtn.setText("Error!");
+                        locationTrack.showSettingsAlert();
+                    }
+                }else{
+                    locationbtn.setText("Share Location");
+                    longitude = 0.0;
+                    latitude = 0.0;
+                }
+
+            }
+        });
+
 
         return view;
     }
 
 
     private void saveData(EntryDetails details) {
-        db.collection("events")
-                .add(details)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+        if(save.getText().toString().contains("Uploading")) {
+            db.collection("events")
+                    .add(details)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            save.setText("Done");
+                            getActivity().getFragmentManager().popBackStack();
 
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error adding document", e);
+                            save.setText("Error!");
+                        }
+                    });
+        }
     }
 
 
 
-    private Bitmap bitmap;
-    private File destination = null;
-    private InputStream inputStreamImg;
-    private String imgPath = null;
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -235,8 +285,8 @@ public class AddFoodDetailsFragment extends Fragment {
                 e.printStackTrace();
             }
         } else if (requestCode == Utils.PICK_IMAGE_GALLERY) {
-            Uri selectedImage = data.getData();
             try {
+                Uri selectedImage = data.getData();
                 bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImage);
                 ByteArrayOutputStream bytes = new ByteArrayOutputStream();
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
@@ -356,9 +406,85 @@ public class AddFoodDetailsFragment extends Fragment {
             e.printStackTrace();
         }
     }
+    private ArrayList<String> findUnAskedPermissions(ArrayList<String> wanted) {
+        ArrayList<String> result = new ArrayList<String>();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+    private boolean hasPermission(String permission) {
+        if (canMakeSmores()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return (ActivityCompat.checkSelfPermission(getContext(),permission) == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return true;
+    }
+
+    private boolean canMakeSmores() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(getActivity())
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try {
+            locationTrack.stopListener();
+        }catch (Exception e){
+
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+
+            case ALL_PERMISSIONS_RESULT:
+                for (String perms : permissionsToRequest) {
+                    if (!hasPermission(perms)) {
+                        permissionsRejected.add(perms);
+                    }
+                }
+
+                if (permissionsRejected.size() > 0) {
 
 
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                            showMessageOKCancel("These permissions are mandatory for the application. Please allow access.",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
+                                            }
+                                        }
+                                    });
+                            return;
+                        }
+                    }
 
+                }
+
+                break;
+        }
+
+    }
 }
 
 
